75db432a8b9bf72ce4bd2df9a3c2433db0a812c8
gkvnlou <nowaru99@gmail.com>
2022-03-22 20:15:55 +0700

    Adding code for W2M1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,59 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
       </header>
+      <body>
+        <div></div>
+        <div class="container">
+            <div class="container-textarea">
+                <h1>Song Player</h1>
+            </div>
+          <img src="./oreo.jpg" class="oreo" />
+          <p></p>
+          <table>
+            <tr>
+              <td>
+                Song Title
+              </td>
+              <td>
+                <div></div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Song Artist
+              </td>
+              <td>
+                P_G2FE2056_Kevin
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Song Album
+                <div></div>
+              </td>
+              <td>
+                Generasi GIGIH 2.0
+              </td>
+            </tr>
+            <tr>
+                <td>
+                  Song Description
+                  <div></div>
+                </td>
+                <td>
+                  <div></div>
+                </td>
+              </tr>
+          </table>
+          <div class="btn">
+            <button>Select</button>
+            <div></div>
+          </div>
+        </div>
+        <script src="src/index.js"></script>
+      </body>
     </div>
   );
 }
